5799916418a45939cdd17250aed7690d49fcb18e
Nimander <szymanskimichal57@gmail.com>
2018-04-23 13:19:43 +0200

    created 6 image buttons

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ from tkinter import *
 class Application(Frame):
 	def __init__(self, master):
 		super(Application, self).__init__(master)
-		self.bttn_clicks = 0
 		self.grid()
-		self.create_widgets()
 
-	def create_widgets(self):
-		self.bttn1 = Button(self, text='Liczba klikniec: 0', command=self.update_count)
-		self.bttn1.grid()
-
-	def update_count(self):
-		self.bttn_clicks += 1
-		self.bttn1['text'] = 'Liczba klikniec: ' + str(self.bttn_clicks)
+		self.img = PhotoImage(file="a.png")
+		self.label = Label(root)
+		self.label.grid()
+		self.bttn = []
+		for i in range(6):
+			self.bttn.append(Button(self.label))
+			self.bttn[i].config(image=self.img)
+			self.bttn[i].grid(row=0, column=i)
 
 
 root = Tk()
