f3c9dc56a62b2c98cbcbef96f9c1826c421bc395
Nimander <szymanskimichal57@gmail.com>
2018-04-24 20:14:40 +0200

    labels change colors correctly according to current turn

--- main.py
+++ main.py
@@ -6,19 +6,27 @@ class Application(Frame):
 	def __init__(self, master):
 		super(Application, self).__init__(master)
 		self.grid()
+		self.red = PhotoImage(file='red.png')
+		self.yellow = PhotoImage(file='yellow.png')
+		self.values = [[0 for i in range(7)] for j in range(6)]
+		self.turn = 1
 
+		print(self.values)
 		self.label_info = Label(self, height=1).grid()
-		self.label_score = Label(self.label_info, text='Wynik 0:0').grid(row=0, column=0, columnspan=2)
-		self.label_turn = Label(self.label_info, text='Tura gracza X').grid(row=0, column=2, columnspan=2)
-		self.button_reset = Button(self.label_info, text='reset').grid(row=0, column=4, columnspan=2)
+		self.label_score = Label(self.label_info, text='Wynik 0:0')
+		self.label_score.grid(row=0, column=0, columnspan=2)
+		self.label_turn = Label(self.label_info, text='Tura gracza X')
+		self.label_turn.grid(row=0, column=2, columnspan=2)
+		self.button_reset = Button(self.label_info, text='reset', command=self.reset)
+		self.button_reset.grid(row=0, column=4, columnspan=2)
 
 		self.label_buttons = Label(self).grid()
 		self.img = PhotoImage(file="a.png")
 		self.bttn = []
-		for i in range(7):
+		for col in range(7):
 			self.bttn.append(Button(self.label_buttons))
-			self.bttn[i].config(image=self.img, command=partial(self.new_move, i))
-			self.bttn[i].grid(row=1, column=i)
+			self.bttn[col].config(image=self.red, command=partial(self.new_move, col))
+			self.bttn[col].grid(row=1, column=col)
 
 		self.label_fields = Label(self).grid()
 		self.img2 = PhotoImage(file="b.png")
@@ -29,13 +37,38 @@ class Application(Frame):
 			for j in range(7):
 				tmp.append(Label(self.label_fields))
 				tmp[j].config(image=self.img2)
-				tmp[j].grid(row=i+2, column=j)
+				tmp[j].grid(row=i + 2, column=j)
 			self.label_field.append(tmp)
 
-		self.red = PhotoImage(file='red.png')
+	def new_move(self, col):
+		if self.legal_move(col):
+			self.update_fields(col, self.calculate_row(col))
 
-	def new_move(self, n):
-		self.label_field[5][n].config(image=self.red)
+	def legal_move(self, col):
+		return True
+
+	def calculate_row(self, col):
+		for row in range(5, -1, -1):
+			if self.values[row][col] is 0:
+				return row
+
+	def update_fields(self, col, row):
+		self.values[row][col] = 1
+		if self.turn == 1:
+			self.label_field[row][col].config(image=self.red)
+			for i in range(7):
+				self.bttn[i].config(image=self.yellow)
+			self.turn = 2
+		else:
+			self.label_field[row][col].config(image=self.yellow)
+			for i in range(7):
+				self.bttn[i].config(image=self.red)
+			self.turn = 1
+
+	def reset(self):
+		for i in range(6):
+			for j in range(7):
+				self.values[i][j] = 0
 
 
 root = Tk()
