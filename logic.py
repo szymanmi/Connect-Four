ec5e97ffe437a52098d2e6b58c71c265c06d6155
Nimander <szymanskimichal57@gmail.com>
2018-04-25 19:18:25 +0200

    just another slightly better version

--- logic.py
+++ logic.py
@@ -1,29 +1,42 @@
 class GameRules:
 	def __init__(self):
 		self.flag = 0
-		self.values = [[0 for i in range(7)] for j in range(6)]
+		self.values = [[-1 for i in range(7)] for j in range(6)]
+		self.turn = 0
 
 	# this method determines whether current move can be executed (column is not full)
-	def legal_move(self, col):
+	def column_not_full(self, clicked_col):
 		# if field in zero row and clicked column is not used yet then this column is not full -> move is legal
-		if self.values[0][col] is 0:
+		if self.values[0][clicked_col] is -1:
 			return True
 		return False
 
 	# this method is calculating the correct row in which coin should be placed depending on picked column
-	def calculate_row(self, col, turn):
+	def calculate_row(self, col):
 		for row in range(5, -1, -1):
-			if self.values[row][col] is 0:
-				self.values[row][col] = turn
+			if self.values[row][col] is -1:
 				return row
 
+	def update_fields(self, row, col):
+		self.values[row][col] = self.turn
+
 	def reset(self):
+		self.turn = 0
 		for i in range(6):
 			for j in range(7):
-				self.values[i][j] = 0
+				self.values[i][j] = -1
 
 	def get_values(self):
 		return self.values
+
+	def change_turn(self):
+		if self.turn == 0:
+			self.turn = 1
+		else:
+			self.turn = 0
+
+	def get_turn(self):
+		return self.turn
 
 
 class ConnectFourRules(GameRules):
@@ -31,24 +44,25 @@ class ConnectFourRules(GameRules):
 		for row in range(6):
 			for first_column in range(4):
 				if self.values[row][first_column] is self.values[row][first_column + 1] is self.values[row][
-					first_column + 2] is self.values[row][first_column + 3] and self.values[row][first_column] is not 0:
+					first_column + 2] is self.values[row][first_column + 3] and self.values[row][
+					first_column] is not -1:
 					return True
 
 		for col in range(7):
 			for row in range(3):
 				if self.values[row][col] is self.values[row + 1][col] is self.values[row + 2][col] is \
-						self.values[row + 3][col] and self.values[row][col] is not 0:
+						self.values[row + 3][col] and self.values[row][col] is not -1:
 					return True
 
 		for row in range(3):
 			for col in range(4):
 				if self.values[row][col] is self.values[row + 1][col + 1] is self.values[row + 2][col + 2] is \
-						self.values[row + 3][col + 3] and self.values[row][col] is not 0:
+						self.values[row + 3][col + 3] and self.values[row][col] is not -1:
 					return True
 
 		for col in range(3, 7):
 			for row in range(3):
 				if self.values[row][col] is self.values[row + 1][col - 1] is self.values[row + 2][col - 2] is \
-						self.values[row + 3][col - 3] and self.values[row][col] is not 0:
+						self.values[row + 3][col - 3] and self.values[row][col] is not -1:
 					return True
 		return False
--- main.py
+++ main.py
@@ -9,36 +9,34 @@ class Application(Frame):
 		super(Application, self).__init__(master)
 		self.grid()
 
-		self.logic = logic.ConnectFourRules()
+		self.game_logic = logic.ConnectFourRules()
 
 		self.img = PhotoImage(file="b.png")
-		self.red = PhotoImage(file='red.png')
-		self.yellow = PhotoImage(file='yellow.png')
+		self.img_player = [PhotoImage(file='red.png'), PhotoImage(file='yellow.png')]
 
 		modes = ['Connect 3', 'Connect 4', 'Connect 5']
 
-		self.turn = 1
-
 		# first main label contains: current score, turn info, reset button
 		self.label_1 = Label(self, height=1).grid()
-		self.label_score = Label(self.label_1, text='Wynik 0:0')
-		self.label_score.grid(row=0, column=0, columnspan=2)
-		self.label_turn = Label(self.label_1, text='Tura gracza 1')
-		self.label_turn.grid(row=0, column=2, columnspan=2)
+		self.label_score = Label(self.label_1, text='Wynik\n0:0')
+		self.label_score.grid(row=0, column=0, columnspan=1)
+		self.label_turn = Label(self.label_1, text='Tura\ngracza\n 0')
+		self.label_turn.grid(row=0, column=1, columnspan=1)
 		self.button_reset = Button(self.label_1, text='reset', command=self.reset)
-		self.button_reset.grid(row=0, column=4, columnspan=1)
-
+		self.button_reset.grid(row=0, column=2, columnspan=1)
 		self.game_mode = StringVar(self.label_1)
 		self.game_mode.set(modes[1])
 		self.w = OptionMenu(self.label_1, self.game_mode, *modes)
+		self.w.grid(row=0, column=3, columnspan=2)
+		self.button_apply = Button(self.label_1, text='zmień tryb', command=self.change_mode)
+		self.button_apply.grid(row=0, column=5, columnspan=2)
 
-		self.w.grid(row=0, column=5, columnspan=2)
 		# second main label contains 7 clickable buttons
 		self.label_2 = Label(self).grid()
 		self.bttn = []
 		for col in range(7):
 			self.bttn.append(Button(self.label_2))
-			self.bttn[col].config(image=self.red, command=partial(self.new_move, col))
+			self.bttn[col].config(image=self.img_player[0], command=partial(self.new_move, col))
 			self.bttn[col].grid(row=1, column=col)
 
 		# third main label contains 6x7 labels representing fields for coins in game
@@ -54,50 +52,39 @@ class Application(Frame):
 			self.label_field.append(tmp)
 
 	# this method is called whenever user clicked on any of 7 buttons
-	def new_move(self, col):
+	def new_move(self, clicked_col):
 		# if move cannot be performed then do nothing
-		if self.logic.legal_move(col):
-			self.update_fields(col, self.logic.calculate_row(col, self.turn))
+		if self.game_logic.column_not_full(clicked_col):
+			self.update_fields(clicked_col, self.game_logic.calculate_row(clicked_col))
 		else:
 			tkinter.messagebox.showinfo(":(", "no tu już się nie zmieści typie")
 
 	# this method is called when user picked correct column to put coin in
 	def update_fields(self, col, row):
-		if self.turn == 1:
-			self.label_field[row][col].config(image=self.red)
+		self.game_logic.update_fields(row, col)
+		self.label_field[row][col].config(image=self.img_player[self.game_logic.get_turn()])
+
+		if not self.game_logic.check_end():
+			self.game_logic.change_turn()
 			for i in range(7):
-				self.bttn[i].config(image=self.yellow)
+				self.bttn[i].config(image=self.img_player[self.game_logic.get_turn()])
+			self.label_turn.config(text='Tura\ngracza\n' + str(self.game_logic.get_turn()))
+
 		else:
-			self.label_field[row][col].config(image=self.yellow)
-			for i in range(7):
-				self.bttn[i].config(image=self.red)
-		if not self.check_end():
-			self.change_turn()
-			self.label_turn.config(text='Tura gracza ' + str(self.turn))
+			tkinter.messagebox.showinfo("koniec", "wygrał gracz " + str(self.game_logic.get_turn()))
+			self.reset()
 
 	def reset(self):
-		self.logic.reset()
-		self.turn = 1
-		self.label_turn.config(text='Tura gracza ' + str(self.turn))
+		self.game_logic.reset()
+		self.label_turn.config(text='Tura\ngracza\n' + str(self.game_logic.get_turn()))
 		for i in range(7):
-			self.bttn[i].config(image=self.red)
+			self.bttn[i].config(image=self.img_player[0])
 		for i in range(6):
 			for j in range(7):
 				self.label_field[i][j].config(image=self.img)
 
-	def check_end(self):
-		if self.logic.check_end():
-			tkinter.messagebox.showinfo("koniec", "wygrał gracz " + str(self.turn))
-			self.reset()
-			return True
-		return False
-
-	def change_turn(self):
-		if self.turn is 1:
-			self.turn = 2
-		else:
-			self.turn = 1
-
+	def change_mode(self):
+		print('aa')
 
 root = Tk()
 root.title('projekcik')
