e02bbe2976c50a4ece50166e8735d9747e35e6dd
Nimander <szymanskimichal57@gmail.com>
2018-04-25 08:56:42 +0200

    added checking for end of the game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.messagebox
 from functools import partial
 
 
@@ -18,7 +19,7 @@ class Application(Frame):
 		self.label_1 = Label(self, height=1).grid()
 		self.label_score = Label(self.label_1, text='Wynik 0:0')
 		self.label_score.grid(row=0, column=0, columnspan=2)
-		self.label_turn = Label(self.label_1, text='Tura gracza X')
+		self.label_turn = Label(self.label_1, text='Tura gracza 1')
 		self.label_turn.grid(row=0, column=2, columnspan=2)
 		self.button_reset = Button(self.label_1, text='reset', command=self.reset)
 		self.button_reset.grid(row=0, column=4, columnspan=2)
@@ -70,20 +71,62 @@ class Application(Frame):
 			self.label_field[row][col].config(image=self.red)
 			for i in range(7):
 				self.bttn[i].config(image=self.yellow)
-			self.turn = 2
 		else:
 			self.label_field[row][col].config(image=self.yellow)
 			for i in range(7):
 				self.bttn[i].config(image=self.red)
-			self.turn = 1
-
-		self.label_turn.config(text='Tura gracza ' + str(self.turn))
+		if not self.check_end():
+			self.change_turn()
+			self.label_turn.config(text='Tura gracza ' + str(self.turn))
 
 	def reset(self):
+		self.turn = 1
+		self.label_turn.config(text='Tura gracza ' + str(self.turn))
+		for i in range(7):
+			self.bttn[i].config(image=self.red)
 		for i in range(6):
 			for j in range(7):
 				self.values[i][j] = 0
 				self.label_field[i][j].config(image=self.img)
+
+	def check_end(self):
+		for row in range(6):
+			for first_column in range(4):
+				if self.values[row][first_column] is self.values[row][first_column + 1] is self.values[row][
+					first_column + 2] is self.values[row][first_column + 3] and self.values[row][first_column] is not 0:
+					tkinter.messagebox.showinfo("Koniec", "Wygrał gracz " + str(self.turn))
+					self.reset()
+					return True
+
+		for col in range(7):
+			for row in range(3):
+				if self.values[row][col] is self.values[row + 1][col] is self.values[row + 2][col] is \
+						self.values[row + 3][col] and self.values[row][col] is not 0:
+					tkinter.messagebox.showinfo("Koniec", "Wygrał gracz " + str(self.turn))
+					self.reset()
+					return True
+
+		for row in range(3):
+			for col in range(4):
+				if self.values[row][col] is self.values[row + 1][col + 1] is self.values[row + 2][col + 2] is \
+						self.values[row + 3][col + 3] and self.values[row][col] is not 0:
+					tkinter.messagebox.showinfo("Koniec", "Wygrał gracz " + str(self.turn))
+					self.reset()
+					return True
+		for col in range(3, 7):
+			for row in range(3):
+				if self.values[row][col] is self.values[row + 1][col - 1] is self.values[row + 2][col - 2] is \
+						self.values[row + 3][col - 3] and self.values[row][col] is not 0:
+					tkinter.messagebox.showinfo("Koniec", "Wygrał gracz " + str(self.turn))
+					self.reset()
+					return True
+		return False
+
+	def change_turn(self):
+		if self.turn is 1:
+			self.turn = 2
+		else:
+			self.turn = 1
 
 
 root = Tk()
